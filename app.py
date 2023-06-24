d1593a02239faf4af781bffc1ddd12d768511ee1
Arthi R <121683354+arthi-rajendran-DS@users.noreply.github.com>
2023-06-24 21:35:53 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,79 +1,53 @@
 import streamlit as st
-import faiss
-from faiss import swigfaiss
-from haystack.nodes import FARMReader, PreProcessor, PDFToTextConverter, DensePassageRetriever
-from haystack.nodes import BM25Retriever
-from haystack.document_stores import FAISSDocumentStore
-from haystack.utils import launch_es
-from haystack.pipelines import ExtractiveQAPipeline
+import os
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain import HuggingFaceHub
+from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.document_loaders import UnstructuredURLLoader
 
-document_store = FAISSDocumentStore(faiss_index_factory_str="Flat")
-document_store.save(index_path="my_index.faiss", config_path="my_config.json")
+def main():
+    st.title("Your PDF Gini")
+    st.subheader("Upload a PDF file and enter your Hugging Face API key")
 
-preprocessor = PreProcessor(
-    clean_empty_lines=True,
-    clean_whitespace=True,
-    clean_header_footer=True,
-    split_by="word",
-    split_length=100,
-    split_respect_sentence_boundary=True,
-    split_overlap=3
-)
+    # Add file upload functionality
+    uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
-@st.cache(show_spinner=False, allow_output_mutation=True)
-def written_document(pdf_file):
-    converter = PDFToTextConverter(remove_numeric_tables=True, valid_languages=["en"])
-    document = [converter.convert(file_path=pdf_file.name, meta=None)[0]]
-    preprocessed_docs = preprocessor.process(document)
-    document_store.write_documents(preprocessed_docs)
-    return None
+    # Add input for Hugging Face API key
+    api_key = st.text_input("Enter your Hugging Face API key")
 
-@st.cache(show_spinner=False, allow_output_mutation=True)
-def predict(question, pdf_file):
-    written_document(pdf_file)
-    retriever = BM25Retriever(document_store)
-    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2")
-    pipe = ExtractiveQAPipeline(reader, retriever)
-    result = pipe.run(query=question, params={"Retriever": {"top_k": 20}, "Reader": {"top_k": 5}})
-    answers = print_answers(result)
-    return answers
+    st.write("How to get your API? https://youtu.be/jo_fTD2H4xA")
 
-def print_answers(results):
-    fields = ["answer", "score"]
-    answers = results["answers"]
-    filtered_answers = []
-    
-    for ans in answers:
-        filtered_ans = {
-            field: getattr(ans, field)
-            for field in fields
-            if getattr(ans, field) is not None
-        }
-        filtered_answers.append(filtered_ans)
+    if uploaded_file is not None and api_key:
+        # Set the Hugging Face API key
+        os.environ["HUGGINGFACEHUB_API_TOKEN"] = api_key
 
-    return filtered_answers
+        # Load the PDF document
+        pdf_loader = UnstructuredPDFLoader(uploaded_file)
+        index = VectorstoreIndexCreator(
+            embedding=HuggingFaceEmbeddings(),
+            text_splitter=CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        ).from_loaders([pdf_loader])
 
-st.set_page_config(page_title="Ponniyin Selvan Chatbot", page_icon=":guardsman:", layout="wide")
+        # Load the retrieval QA chain
+        llm2 = HuggingFaceHub(repo_id="declare-lab/flan-alpaca-large", model_kwargs={"temperature": 0, "max_length": 1512})
+        chain = RetrievalQA.from_chain_type(
+            llm=llm2,
+            chain_type="stuff",
+            retriever=index.vectorstore.as_retriever(),
+            input_key="question"
+        )
 
-st.title("Ponniyin Selvan Chatbot")
-st.markdown("<center><b>Sample Questions:</b> Who is Vandia Devan?</center>", unsafe_allow_html=True)
+        # Run the question answering chain
+        chain.run()
 
-question = st.text_area("Ask an open question!", height=100)
+        # Display the results
+        st.subheader("PDF says:")
+        st.write(chain.results)
 
-pdf_file = st.file_uploader("Upload a PDF", type=["pdf"])
-
-if st.button("Ask"):
-    if not pdf_file:
-        st.warning("Please upload a PDF file.")
-    elif not question:
-        st.warning("Please enter a question.")
-    else:
-        answers = predict(question, pdf_file)
-        if answers:
-            st.markdown("<h2>Answers:</h2>", unsafe_allow_html=True)
-            for ans in answers:
-                st.write(f"Answer: {ans['answer']}")
-                st.write(f"Score: {ans['score']}")
-                st.write("---")
-        else:
-            st.warning("No answer found.")
+if __name__ == "__main__":
+    main()
